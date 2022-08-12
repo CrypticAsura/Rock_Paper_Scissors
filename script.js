3a7f61cc7d152fab3e6fc28a3b4085407e753fff
CrypticAsura <ilbulgak@gmail.com>
2022-08-12 17:24:17 -0500

    add round play function

--- script.js
+++ script.js
@@ -20,3 +20,36 @@ function getPlayerChoice() {
     }
     return hand;
 }
+
+function gameRound(rounds) {
+    let winCount = 0;
+    for (let i=0; i < rounds; i++) {
+    let computerChoice = getComputerChoice();
+    let playerChoice = getPlayerChoice();
+    if (computerChoice == playerChoice) {
+        console.log("This is a tie");
+        i--
+    } else if (playerChoice == null) {
+        console.log("Play the goddamn game!");
+        i--
+    } else if ((computerChoice == "rock")&&(playerChoice=="scissors")) {
+        console.log("You lose!");
+    } else if ((computerChoice == "paper")&&(playerChoice=="rock")) {
+        console.log("You lose!");
+    } else if ((computerChoice == "scissors")&&(playerChoice=="paper")) {
+        console.log("You lose!");
+    } else {
+        console.log("You win!");
+        winCount++
+    };
+    console.log(`You played ${playerChoice} and computer played ${computerChoice}`)
+    if (winCount/rounds > .5) {
+        console.log(`You've won best out of ${rounds}. Good job.`)
+        return;
+    } else if ((i+1-winCount)/rounds>.5) {
+        console.log("Fucking loser");
+        return;
+    };
+    };
+};
+gameRound(5);
